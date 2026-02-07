369ca9a2844bbf469353ca2e03e4765b2ff0b7c7
Lisardowo <lisardowo@gmail.com>
2026-02-06 22:23:59 -0600

    Implemented a correct message transmision with static buffer

--- src/transmiter.c
+++ src/transmiter.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 //#include <sx128x.h>
 
-#define MESSAGE_SIZE 65
+#define MESSAGE_SIZE 108
 
 int ID;
-int recieverID;
+int receiverID;
 int transmitterID;
 char message[MESSAGE_SIZE];
 
-int sendMessage(int recieverID, int emmisorID,char *message,struct sender *debugSender,struct receiver *debugReciever);
-int getReciever();
-int validateComms();
+struct device{ // change struct name to something more explicit
+
+    int emissorID;
+    int receiverID;
+    char message[MESSAGE_SIZE];
+    //look for implemetntaion of enums in structures
+
+
+} ;
+
+
+
+struct device debugReceiver;
+struct device debugSender;
+
+int sendMessage(int receiverID, int emmisorID,char *message,struct device *debugSender,struct device *debugReceiver);
+int getreceiver();
+int validateConnection();
+int setMessage(char *message,struct device *debugReceiver );
 
 enum metadata{
     
@@ -21,75 +39,55 @@ enum metadata{
 };
 
 
-struct sender{
-
-    int ID;
-    int recieverID;
-    char message[MESSAGE_SIZE];
-    //look for implemetntaion of enums in structures
-
-
-} ;
-
-struct receiver{
-    int ID;
-    int transmitterID;
-    char message[MESSAGE_SIZE];
-
-}; // will this be redundant?
-
-struct message{
-
-    
-
-};
-
-
 
 
 int main(){
 
-    int recieverID;
+    char debug[100];
+    int n = 10;
+
+    int receiverID;
     transmitterID = 4;
-    recieverID = getReciever(); 
-    printf("Provided ID is : %d \n", recieverID);
+    receiverID = getreceiver(); 
+    printf("Provided ID is : %d \n", receiverID);
 
     //message
-    printf("provide the message");
-    scanf("%64s", message);
-    printf("message is : %s", message);//TODO set buffer size to prevent bufffer overflows ==> Raise Error for long messages
-
-    
-    //construction of structs
-    struct receiver debugReceiver;
-    struct sender debugSender;
+    printf("provide the message : \n");
+    scanf("%*c");
+    fgets(message, MESSAGE_SIZE, stdin);
+    printf("message is : %s\n", message);//TODO set buffer size to prevent bufffer overflows ==> Raise Error for long messages
 
     // initialization of transmitter
     
+    debugSender.emissorID = transmitterID;
+    debugSender.receiverID = receiverID;
+    strncpy(debugSender.message, message, MESSAGE_SIZE);
+
+    // initialization of receiver
     
-    debugSender.ID = ID;
-    debugSender.recieverID = recieverID;
+    debugReceiver.emissorID = transmitterID;
+    debugReceiver.receiverID = receiverID;
 
-    // initialization of reciever
     
-    debugReceiver.ID = recieverID;
-    debugReceiver.transmitterID = transmitterID;
+    int succes = sendMessage(receiverID, transmitterID, message, &debugSender, &debugReceiver);
 
-
-    if(sendMessage(recieverID, transmitterID, message, &debugSender, &debugReceiver) == 0){
-        printf("OK");
+    if(succes == 0){
+        printf("OK\n");
+    }
+    else{
+        printf("%d\n", succes);
     }
 
 }
 
-int getReciever(){
+int getreceiver(){
 
-    int recieverID;
-    printf("insert an ID");
-    scanf("%d", &recieverID);
-    printf("%d\n", recieverID);
+    int receiverID;
+    printf("insert an ID\n");
+    scanf("%d", &receiverID);
+    printf("Receiver %d\n", receiverID);
 
-    return recieverID;
+    return receiverID;
 
 
 
@@ -99,24 +97,49 @@ int getReciever(){
 //char getMessage(){} //TODO get message via function
 
 //errors 200 -> NOT possible Communications (user associated)
-int sendMessage(int recieverID, int emmisorID,char *message,struct sender *debugSender,struct receiver *debugReciever){
+//errors 300 -> Message Couldnt be send
+int sendMessage(int receiverID, int emmisorID,char *message,struct device *debugSender,struct device *debugReceiver){
 
-    if (debugSender->ID || debugReciever->transmitterID != emmisorID){
+    printf("sendingMessage\n");
+    if (debugSender->emissorID != emmisorID || debugReceiver->emissorID != emmisorID){
+
+        printf("Emisor = %d", emmisorID);
+        printf("Sender = %d  ", debugSender->emissorID);
+        printf("Receiver = %d  \n", debugReceiver->emissorID);
 
         return 201; // Error 201 -> Not a valid emmisor ID
 
     }
-    if(debugSender->recieverID || debugReciever->ID != recieverID){
-        return 202;// Error 202 -> Not a valid reciever ID
+    if(debugSender->receiverID != receiverID || debugReceiver->receiverID != receiverID){
+        
+        printf("receiver = %d ", receiverID);
+        printf("Sender = %d  ", debugSender->receiverID);
+        printf("Receiver = %d  \n", debugReceiver->receiverID);
+
+        return 202;// Error 202 -> Not a valid receiver ID
     }
+    else{
+        printf("Sender Message = %s ", debugSender->message);
+        printf("Receiver Message = %s \n", debugReceiver->message);
+        
+        if(setMessage(message, debugReceiver) != 0){
 
-
-
-    return 0;
+            return 301;
+        }
+        printf("Received Message = %s \n", debugReceiver->message);
+        return 0;
+    }    
 
 
 }
 
-int validateComms(){
+int setMessage(char *message,struct device *debugReceiver ){
+    
+    strncpy(debugReceiver->message, message, MESSAGE_SIZE);
+    return 0;
+}
 
+int ValidateConnection(){
+
+    return 0;
 } //TODO refactorize the validation logic in send To validate comms to keep a cleaner code
